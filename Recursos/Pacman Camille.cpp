c0a3432357d7cd509a58761baecb36e08915180b
kavim0 <kamilvidalmo@gmail.com>
2025-10-09 18:40:42 -0600

    actualizacion

--- Recursos/Pacman Camille.cpp
+++ Recursos/Pacman Camille.cpp
@@ -1,14 +1,22 @@
 //#include <allegro.h>
-#include <conio.h>
+//#include <conio.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+//#include <windows.h>
+#include <allegro.h>
 
+#define delayRefresh 0.3 //segundos;
+
+void init (); 
 void cargarmapa1(int matrizjuego[][30]);
 void motordejuego();
-void imprimirMapa(int matrizjuego[][30]);
+void imprimirMapa(int matrizjuego[20][30], BITMAP *buffer);
 void movimientoPacman(int matrizjuego[][30]);
 void IFmove(int matrizjuego[][30], char tecla, int posPacman[2]);
+void spawnFruta(int matrizjuego[20][30]);
+void OcultaCursor();
+
 /*0 -> PACMAN
 1 -> Bloque
 2 -> Espacio libre 
@@ -21,14 +29,37 @@ void IFmove(int matrizjuego[][30], char tecla, int posPacman[2]);
 9 -> Clyde -> NARANJA -> SE MUEVE AL AZAR RAPIDO*/
 
 int main(){
+	srand(time(NULL)); //declarar random
+	init();
 	motordejuego();
-	
+	//deinit();
 	return 0;
+}
+END_OF_MAIN();
+
+void init () { //inicializar allegro
+	
+	int depth, res,cancion;
+	allegro_init();
+	depth = desktop_color_depth();
+	if (depth == 0) depth = 32;
+	set_color_depth(depth);
+	res = set_gfx_mode(GFX_AUTODETECT_WINDOWED, 900, 635, 0, 0);
+	if (res != 0) {
+		allegro_message(allegro_error);
+		exit(-1);
+	}
+	set_window_title("Pac_Man Game");
+
+	install_timer();
+	install_keyboard();
+	install_mouse();
+	show_mouse(screen);
 }
 
 void cargarmapa1(int matrizjuego[20][30]){
 	int i,j;
-
+//declarar mapa
 	int mapa[20][30]{
 		//1 2 3 4 5 6 7 8 9 
 		{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},//1
@@ -39,10 +70,10 @@ void cargarmapa1(int matrizjuego[20][30]){
 		{1,2,2,2,2,2,2,1,2,1,1,2,1,2,2,2,2,1,2,1,1,2,1,4,2,2,2,2,2,1},//6
 		{1,2,1,1,2,1,1,1,2,2,1,2,1,2,1,1,2,1,2,1,2,2,1,1,1,2,1,1,2,1},//7
 		{1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1},//8
-		{1,2,1,1,1,2,1,2,1,1,1,1,2,3,3,3,3,2,1,1,1,1,2,1,2,1,1,1,2,1},//9
-		{1,2,1,1,1,2,1,2,1,1,1,1,2,3,9,8,3,2,1,1,1,1,2,1,2,1,1,1,2,1},//10
-		{2,2,2,2,2,2,1,2,2,1,1,1,2,3,10,7,3,2,1,1,1,2,2,1,2,2,2,2,2,2},//11
-		{1,2,1,1,1,2,1,1,2,2,2,2,2,3,3,3,3,2,2,2,2,2,1,1,2,1,1,1,2,1},//12
+		{1,2,1,1,1,2,1,2,1,1,1,1,2,1,1,1,1,2,1,1,1,1,2,1,2,1,1,1,2,1},//9
+		{1,2,1,1,1,2,1,2,1,1,1,1,2,1,9,8,1,2,1,1,1,1,2,1,2,1,1,1,2,1},//10
+		{2,2,2,2,2,2,1,2,2,1,1,1,2,1,10,7,1,2,1,1,1,2,2,1,2,2,2,2,2,2},//11
+		{1,2,1,1,1,2,1,1,2,2,2,2,2,1,1,1,1,2,2,2,2,2,1,1,2,1,1,1,2,1},//12
 		{1,2,1,2,2,2,2,2,2,1,1,1,2,2,2,2,2,2,1,1,1,2,2,2,2,2,2,1,2,1},//13
 		{1,2,1,2,1,1,2,1,1,1,1,1,2,1,1,1,1,2,1,1,1,1,1,2,1,1,2,1,2,1},//14
 		{1,2,1,2,1,2,2,2,1,2,2,2,2,2,0,2,2,2,2,2,2,1,2,2,2,1,2,1,2,1},//15
@@ -52,7 +83,7 @@ void cargarmapa1(int matrizjuego[20][30]){
 		{1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1},//19
 		{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},//20
 	};
-	
+//pasa mapa 1 a matriz juego
 	for (i=0; i<=19; i++){
 		for (j=0; j<=29; j++){
 			matrizjuego[i][j]=mapa[i][j];
@@ -64,21 +95,30 @@ void cargarmapa1(int matrizjuego[20][30]){
 
 void motordejuego(){
 	
-	int matrizjuego[20][30];
+	int matrizjuego[20][30]; //declarar matriz juego
 	
-	cargarmapa1(matrizjuego);
+	OcultaCursor(); //oculta cursor
 	
-	imprimirMapa(matrizjuego);
-	
+	cargarmapa1(matrizjuego); //declara mapa 1 y pasa mapa 1 a matriz juego
+		BITMAP *buffer = create_bitmap(960,660);
+
+	imprimirMapa(matrizjuego,buffer); //imprime matriz juego
 	movimientoPacman(matrizjuego);
+	
+	spawnFruta(matrizjuego); //genera fruta de forma aleatoria
 	
 }
 
-void imprimirMapa(int matrizjuego[20][30]){
+void imprimirMapa(int matrizjuego[20][30], BITMAP *buffer){
 	int i,e;
+	BITMAP *vectorMapa[15];
+	vectorMapa[0] = load_bitmap("CuerpoPacman_II.bmp",NULL);
 	
 	for(i=0;i<20;i++){
 		for(e=0;e<30;e++){
+			if(matrizjuego[i][e] == 0){
+				draw_sprite(buffer,vectorMapa[0],e*30,i*30+35);
+			}
 			printf("%d ",matrizjuego[i][e]);
 		}
 		printf("\n");
@@ -88,19 +128,18 @@ void imprimirMapa(int matrizjuego[20][30]){
 
 void movimientoPacman(int matrizjuego[20][30]){
 	int posPacman[2]={14, 14};
-	float delayRefresh=1; //segundos;
+	//float 
 	char tecla;
 	do{
-		clock_t tstart = clock();
-		while((clock() - tstart) / CLOCKS_PER_SEC < delayRefresh){
+		/*clock_t tstart = clock();
+		while((clock() - tstart) / CLOCKS_PER_SEC < delayRefresh){ //ingresa la tecla de movimiento del usuario
 			if(_kbhit()){
 	     		tecla=_getch();
 			}
-		}
-		IFmove(matrizjuego, tecla, posPacman);
-		//no jalo >:( int matrizjuego[20][30];
+		}*/
+		IFmove(matrizjuego, tecla, posPacman); //comprueba la tecla de movimiento del usuario
 		system("cls");
-		imprimirMapa(matrizjuego);
+		//imprimirMapa(matrizjuego);
 	}while(tecla != '1');
 }
 
@@ -128,4 +167,23 @@ void IFmove(int matrizjuego[20][30],char tecla, int posPacman[2]){
 			break;
 	}
 	printf("\n%d, %d",posPacman[0],posPacman[1]);//debug
+}
+
+/*void OcultaCursor(){ // Oculta la flechita el cuadro que parpadea en el ejecutable y se pone antes de la generacion de cualquier cosa.
+	CONSOLE_CURSOR_INFO cci = {100, FALSE};
+	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cci);
+}*/
+
+void spawnFruta(int matrizjuego[20][30]){
+	int frutaPosX, frutaPosY, flag=0;
+	
+	while(flag!=1){
+		frutaPosX = rand() % (20-1 + 1);
+		frutaPosY = rand() % (30-1 + 1);
+		if(matrizjuego[frutaPosX][frutaPosY] == 4){
+			matrizjuego[frutaPosX][frutaPosY] == 11;
+			flag++;
+			break;
+		}
+	}
 }
