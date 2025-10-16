caf6e1caa5b3de9b28b7f9971caca02affd16757
kavim0 <kamilvidalmo@gmail.com>
2025-10-15 18:37:18 -0600

    actualizacion

--- Recursos/Pacman Camille.cpp
+++ Recursos/Pacman Camille.cpp
@@ -13,15 +13,15 @@ void cargarmapa1(int matrizjuego[][30]);
 void motordejuego();
 void imprimirMapa(int matrizjuego[20][30], BITMAP *buffer);
 void movimientoPacman(int matrizjuego[][30]);
-void IFmove(int matrizjuego[][30], char tecla, int posPacman[2]);
+void IFmove(int matrizjuego[][30], int tecla, int posPacman[2]);
 void spawnFruta(int matrizjuego[20][30]);
 void OcultaCursor();
 
 /*0 -> PACMAN
 1 -> Bloque
-2 -> Espacio libre 
+2 -> Punto chico
 3 -> Fruta
-4 -> Punto chico
+4 -> Espacio libre
 5 -> punto grande 
 6 -> Blinky -> ROJO  -> PERSIGUE LOCAMENTE AL PACMAN 
 7 -> Inky -> AZULITO  -> SE MUEVE AL AZAR PERO MÁS LENTO
@@ -67,12 +67,12 @@ void cargarmapa1(int matrizjuego[20][30]){
 		{1,2,1,2,1,1,2,1,1,1,1,2,1,2,1,1,2,1,2,1,1,1,1,2,1,1,2,1,2,1},//3
 		{1,2,1,2,1,1,2,1,1,2,2,2,1,2,2,2,2,1,2,2,2,1,1,2,1,1,2,1,2,1},//4
 		{1,2,1,2,1,1,2,2,2,2,1,2,1,2,1,1,2,1,2,1,2,2,2,2,1,1,2,1,2,1},//5
-		{1,2,2,2,2,2,2,1,2,1,1,2,1,2,2,2,2,1,2,1,1,2,1,4,2,2,2,2,2,1},//6
+		{1,2,2,2,2,2,2,1,2,1,1,2,1,2,2,2,2,1,2,1,1,2,1,2,2,2,2,2,2,1},//6
 		{1,2,1,1,2,1,1,1,2,2,1,2,1,2,1,1,2,1,2,1,2,2,1,1,1,2,1,1,2,1},//7
 		{1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1},//8
 		{1,2,1,1,1,2,1,2,1,1,1,1,2,1,1,1,1,2,1,1,1,1,2,1,2,1,1,1,2,1},//9
-		{1,2,1,1,1,2,1,2,1,1,1,1,2,1,9,8,1,2,1,1,1,1,2,1,2,1,1,1,2,1},//10
-		{2,2,2,2,2,2,1,2,2,1,1,1,2,1,10,7,1,2,1,1,1,2,2,1,2,2,2,2,2,2},//11
+		{1,2,1,1,1,2,1,2,1,1,1,1,2,1,8,7,1,2,1,1,1,1,2,1,2,1,1,1,2,1},//10
+		{2,2,2,2,2,2,1,2,2,1,1,1,2,1,9,6,1,2,1,1,1,2,2,1,2,2,2,2,2,2},//11
 		{1,2,1,1,1,2,1,1,2,2,2,2,2,1,1,1,1,2,2,2,2,2,1,1,2,1,1,1,2,1},//12
 		{1,2,1,2,2,2,2,2,2,1,1,1,2,2,2,2,2,2,1,1,1,2,2,2,2,2,2,1,2,1},//13
 		{1,2,1,2,1,1,2,1,1,1,1,1,2,1,1,1,1,2,1,1,1,1,1,2,1,1,2,1,2,1},//14
@@ -101,15 +101,15 @@ void motordejuego(){
 	
 	cargarmapa1(matrizjuego); //declara mapa 1 y pasa mapa 1 a matriz juego
 	BITMAP *buffer = create_bitmap(960,660);
-
+	
+	spawnFruta(matrizjuego); //genera fruta de forma aleatoria
 	do{
 		imprimirMapa(matrizjuego,buffer); //imprime matriz juego
+		movimientoPacman(matrizjuego);
 		blit(buffer,screen,0,0,0,0,960,660);
 	}while(true); //CICLO INFINITO
 
-	//movimientoPacman(matrizjuego);
-	
-	//spawnFruta(matrizjuego); //genera fruta de forma aleatoria
+	//
 	
 }
 
@@ -118,65 +118,52 @@ void imprimirMapa(int matrizjuego[20][30], BITMAP *buffer){
 	BITMAP *vectorMapa[15];
 	vectorMapa[0] = load_bitmap("CuerpoPacman_II.bmp",NULL);
 	vectorMapa[1] = load_bitmap("Bloques_1.bmp",NULL);
+	vectorMapa[2] = load_bitmap("PuntosChicos.bmp",NULL);
+	vectorMapa[3] = load_bitmap("Fruta 1.bmp",NULL);
+	vectorMapa[4] = load_bitmap("Espacio.bmp",NULL);
+	vectorMapa[5] = load_bitmap("PuntosGrandes.bmp",NULL);
+	vectorMapa[6] = load_bitmap("FantasmaBlinky_I.bmp",NULL);
+	vectorMapa[7] = load_bitmap("FantasmaInky_I.bmp",NULL);
+	vectorMapa[8] = load_bitmap("FantasmaPinky_I.bmp",NULL);
+	vectorMapa[9] = load_bitmap("FantasmaClyde_I.bmp",NULL);
 	for(i=0;i<20;i++){
 		for(e=0;e<30;e++){
-			switch(matrizjuego[i][e]){
-				case 0:
-					draw_sprite(buffer,vectorMapa[0],e*30,i*30+35);
-				break;
-				case 1:
-					draw_sprite(buffer,vectorMapa[1],e*30,i*30+35);
-					break;
-			}
-		
-			//printf("%d ",matrizjuego[i][e]);
+			draw_sprite(buffer,vectorMapa[matrizjuego[i][e]],e*30,i*30+35);
 		}
-		//printf("\n");
 	}
 	
 }
 
 void movimientoPacman(int matrizjuego[20][30]){
 	int posPacman[2]={14, 14};
-	//float 
-	char tecla;
-	do{
-		/*clock_t tstart = clock();
-		while((clock() - tstart) / CLOCKS_PER_SEC < delayRefresh){ //ingresa la tecla de movimiento del usuario
-			if(_kbhit()){
-	     		tecla=_getch();
-			}
-		}*/
-		IFmove(matrizjuego, tecla, posPacman); //comprueba la tecla de movimiento del usuario
-		system("cls");
-		//imprimirMapa(matrizjuego);
-	}while(tecla != '1');
+	//float
+	int tecla = readkey() >> 8;
+	IFmove(matrizjuego, tecla, posPacman); //comprueba la tecla de movimiento del usuario
 }
 
-void IFmove(int matrizjuego[20][30],char tecla, int posPacman[2]){
+void IFmove(int matrizjuego[20][30],int tecla, int posPacman[2]){
 	switch(tecla){
-		case 'w':
+		case KEY_UP:
 			matrizjuego[posPacman[0]][posPacman[1]] = 2;
 			posPacman[0]--;
 			matrizjuego[posPacman[0]][posPacman[1]] = 0;
 			break;
-		case 'a':
+		case KEY_LEFT:
 			matrizjuego[posPacman[0]][posPacman[1]] = 2;
 			posPacman[1]--;
 			matrizjuego[posPacman[0]][posPacman[1]] = 0;
 			break;
-		case 's':
+		case KEY_DOWN:
 			matrizjuego[posPacman[0]][posPacman[1]] = 2;
 			posPacman[0]++;
 			matrizjuego[posPacman[0]][posPacman[1]] = 0;
 			break;
-		case 'd':
+		case KEY_RIGHT:
 			matrizjuego[posPacman[0]][posPacman[1]] = 2;
 			posPacman[1]++;
 			matrizjuego[posPacman[0]][posPacman[1]] = 0;
 			break;
 	}
-	printf("\n%d, %d",posPacman[0],posPacman[1]);//debug
 }
 
 /*void OcultaCursor(){ // Oculta la flechita el cuadro que parpadea en el ejecutable y se pone antes de la generacion de cualquier cosa.
@@ -190,8 +177,8 @@ void spawnFruta(int matrizjuego[20][30]){
 	while(flag!=1){
 		frutaPosX = rand() % (20-1 + 1);
 		frutaPosY = rand() % (30-1 + 1);
-		if(matrizjuego[frutaPosX][frutaPosY] == 4){
-			matrizjuego[frutaPosX][frutaPosY] == 11;
+		if(matrizjuego[frutaPosX][frutaPosY] == 2){
+			matrizjuego[frutaPosX][frutaPosY]=3;
 			flag++;
 			break;
 		}
